7f5b8ed1890515e84dac516e9464b7ed0c830090
NeonLife (Owner) ItssJxstn <113269208+NeonLifeRPde@users.noreply.github.com>
2022-12-05 16:19:36 +0100

    Update server.lua

--- [QB]/rw_hud_i/server.lua
+++ [QB]/rw_hud_i/server.lua
@@ -12,16 +12,16 @@ AddEventHandler('rw_hud:getServerInfo', function()
 	local date = '00/00/2022'
 
 
-		time = os.date("%H:%M", os.time() + 1 * 60 * 60 )
-		date = os.date("%d/%m/%Y",os.time() + 1 * 60 * 60 ) 
+		time = os.date("%H:%M", os.time() + 0 * 60 * 60 )
+		date = os.date("%d/%m/%Y",os.time() + 0 * 60 * 60 ) 
 
 		Citizen.Wait(100)
 
 		local info = {
 			money = Player.PlayerData.money.cash,
 			bank = Player.PlayerData.money.bank,
-			blackm = Player.PlayerData.money.crypto, 
-			dnates = Player.PlayerData.money.donate, 
+--[[ 			blackm = Player.PlayerData.money.crypto,  ]]
+			dnates = Player.PlayerData.money.donate,
 			time = time,
 			date = date
 		}
